93f57709040a2370b22f31bcb69cd121930ebc7d
imdomlol <pwndragon@yahoo.com>
2026-03-09 18:37:22 -0700

    Implemented event class, add event class function, and event save db testing tool

--- models/event.py
+++ models/event.py
@@ -1,21 +1,31 @@
 from typing import Any
 
-class Events:
-    def __init__(self, supabase_client):
+class Event:
+    def __init__(self, title: str, supabase_client: Any, calendar_ids: list, description: str = None, start_timestamp: str = None, end_timestamp: str = None,) -> None:
+        self.id = None  # Will be set when the event is saved to the database
         self.supabase_client = supabase_client
+        self.title = title
+        self.calendar_ids = calendar_ids
+        self.description = description
+        self.start_timestamp = start_timestamp
+        self.end_timestamp = end_timestamp
+        self.age_timestamp = None  # May be returned by DB on insert
 
-    def to_record(self, calendar_id: str, title: str, description: str, start_time: str, end_time: str) -> dict:
+    def to_record(self) -> dict:
         return {
-            "calendar_id": calendar_id,
-            "title": title,
-            "description": description,
-            "start_time": start_time,
-            "end_time": end_time,
+            "id": self.id,
+            "calendar_ids": self.calendar_ids,
+            "title": self.title,
+            "description": self.description,
+            "start_timestamp": self.start_timestamp,
+            "end_timestamp": self.end_timestamp,
+            "age_timestamp": self.age_timestamp,
         }
-    
-    def save(self, calendar_id: str, title: str, description: str, start_time: str, end_time: str) -> Any:
-        record = self.to_record(calendar_id, title, description, start_time, end_time)
-        return self.supabase_client.table("events").insert(record).execute()
-    
-    #id, calendar_id, title, description, start_at, end_at, created_at
-    #id, calendar_ids, title, description, start_timestamp, end_timestamp, age_timestamp
+
+    def save(self) -> Any:
+        record = self.to_record()
+        result = self.supabase_client.table("events").insert(record).execute()
+
+        return result
+
+    # id, calendar_ids, title, description, start_timestamp, end_timestamp, age_timestamp
